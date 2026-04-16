cd6e5fbbc943e5ed3da23f07275203b4bd1d7b21
MaorKikoz <maorkikoz2@gmail.com>
2026-04-17 00:53:01 +0300

    add "local storage"

--- js/service.js
+++ js/service.js
@@ -1,10 +1,16 @@
 'use strict'
 
-let books = [
+const BOOK_KEY = 'bookDB'
+
+let books = loadFromStorage(BOOK_KEY) || createBooks()
+
+function createBooks() {
+  return [
     { id: 'bg4J78', title: 'The adventures of Lori Ipsi', price: 120 },
     { id: 'bg4J79', title: 'The adventures of Lori Ipsi 2', price: 121 },
     { id: 'bg4J80', title: 'The adventures of Lori Ipsi  3', price: 122 }
 ]
+}
 
 
 function getBooks() {
@@ -18,16 +24,25 @@ function getBook(id) {
 
 function removeBook(id) {
   books = books.filter(book => book.id !== id)
+  saveBooks()
 }
 
 function updatePrice(id, newPrice) {
   console.log(id, newPrice);
   var book = getBook(id)
   book.price = newPrice
+
+  saveBooks()
 }
 
 function addBook(title, price) {
   var newId = makeId()
   var newBook = { id: newId, title: title, price: price }
   books.push(newBook)
+
+  saveBooks()
+}
+
+function saveBooks() {
+  saveToStorage(BOOK_KEY, books)
 }
